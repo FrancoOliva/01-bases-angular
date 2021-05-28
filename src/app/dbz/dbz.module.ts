6097eec760b2151da97043f46d9c9873b84cd215
Franco Oliva <franco.oliva1991@gmail.com>
2021-05-28 01:31:58 -0300

    Comenzamos sección 5, continuar con la clase ngModel

---
+++ src/app/dbz/dbz.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { MainPaigeComponent } from './main-paige/main-paige.component';
+
+
+
+@NgModule({
+  declarations: [
+    MainPaigeComponent
+  ],
+  exports:[
+    MainPaigeComponent
+  ],
+  imports: [
+    CommonModule,
+    FormsModule
+  ]
+})
+export class DbzModule {
+  
+ }
